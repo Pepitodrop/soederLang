a2a0fa6f47f4ffc481bd1bf54af1d62585dd7df8
Pepitodrop <66082927+Pepitodrop@users.noreply.github.com>
2026-07-20 08:13:31 +0200

    Compile functions call frames and heap operations

--- src/compiler.js
+++ src/compiler.js
@@ -14,9 +14,47 @@ export function compile(program) {
   const instructions = [];
   const variables = Object.fromEntries(program.data.map((item) => [item.name, item.value]));
   const labels = new Set(program.body.filter((item) => item.type === 'LabelStatement').map((item) => item.name));
+  const functions = new Set(program.body.filter((item) => item.type === 'FunctionStatement').map((item) => item.name));
+  const hasFunctions = functions.size > 0;
+  const hasMain = program.body.some((item) => item.type === 'MainStatement');
+
+  if (hasFunctions) {
+    if (!hasMain) throw new Error('HAUPTPROGRAMM fehlt bei Verwendung von FUNKTION.');
+    instructions.push({ op: 'JMP', target: '__MAIN', line: 0 });
+  }
 
   for (const statement of program.body) {
     switch (statement.type) {
+      case 'FunctionStatement':
+        instructions.push({ op: 'LABEL', name: `__FUNC_${statement.name}`, line: statement.line });
+        break;
+      case 'MainStatement':
+        instructions.push({ op: 'LABEL', name: '__MAIN', line: statement.line });
+        break;
+      case 'ReturnStatement':
+        instructions.push({ op: 'RET', line: statement.line });
+        break;
+      case 'CallStatement':
+        if (!functions.has(statement.target)) throw new Error(`Unbekannte Funktion ${statement.target} in Zeile ${statement.line}`);
+        instructions.push({ op: 'CALL', target: `__FUNC_${statement.target}`, line: statement.line });
+        break;
+      case 'AllocateStatement':
+        requireVariable(variables, statement.target, statement.line);
+        emitValue(instructions, variables, statement.size, statement.line);
+        instructions.push({ op: 'ALLOC', line: statement.line });
+        instructions.push({ op: 'STORE', name: statement.target, line: statement.line });
+        break;
+      case 'HeapWriteStatement':
+        emitValue(instructions, variables, statement.address, statement.line);
+        emitValue(instructions, variables, statement.value, statement.line);
+        instructions.push({ op: 'HSTORE', line: statement.line });
+        break;
+      case 'HeapReadStatement':
+        requireVariable(variables, statement.target, statement.line);
+        emitValue(instructions, variables, statement.address, statement.line);
+        instructions.push({ op: 'HLOAD', line: statement.line });
+        instructions.push({ op: 'STORE', name: statement.target, line: statement.line });
+        break;
       case 'LabelStatement':
         instructions.push({ op: 'LABEL', name: statement.name, line: statement.line });
         break;
