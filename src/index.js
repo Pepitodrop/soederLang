ec39310f28e4ae83496a6da78ccc6856ae7cc2f7
Pepitodrop <66082927+Pepitodrop@users.noreply.github.com>
2026-07-20 08:30:33 +0200

    feat: export speech alias API

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { tokenize } from './tokenizer.js';
 export { parse } from './parser.js';
 export { compile } from './compiler.js';
 export { SoederVM, execute } from './vm.js';
+export { SOEDER_ALIASES, normalizeAliases } from './aliases.js';
